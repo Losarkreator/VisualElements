cf6438e3ed35e648df9c13225920575696853de7
Carlos Manuel Turrion <cturrion@deloitte.es>
2022-10-17 12:49:16 +0200

    Functional

--- VisualElements/Llamada/Vista.swift
+++ VisualElements/Llamada/Vista.swift
@@ -12,32 +12,17 @@ struct Vista: View {
         VStack(alignment: .center) {
             Spacer()
             
-            //TODO: Poner la misma imagen del Pokemon que se muestra
             AsyncImageView(urlSprite: viewModel.pokemonImage.front_default)
             
-            HStack {
-                Text("pokemon Image URL: \n \(viewModel.pokemonImage.front_default)")
-                Spacer()
-            }
-            .padding()
-            
-            Divider()
-                .padding()
-            
             Text(viewModel.pokemon[viewModel.randomNumber].name.capitalized)
-            //Text(viewModel.pokemon[0].url)
-            //Text(viewModel.pokemon[viewModel.randomNumber].url)
-            //Text("\(viewModel.thisThat.that)")
             
-            Spacer()
-            
-            Button("Obtener Nombre") {
+            Button("Obtener Pokemon") {
                 viewModel.getPokemonName()
-//                viewModel.getPokemonSprite()
                 print(viewModel.pokemonImage.front_default)
-                // viewModel.getData()
             }
             .buttonStyle(CambiarColorAlPulsar())
+            
+            Spacer()
             
         }
         .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
@@ -49,7 +34,6 @@ struct Vista: View {
     
     private func getPokemon() {
             viewModel.getPokemonName()
-//            viewModel.getPokemonSprite()
     }
 }
 
